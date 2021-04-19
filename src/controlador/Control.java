d4d60ac2ec3c92fdfdf6ac8f1c82de7c004b829f
baxx1199 <joshuajulcobaut@gmail.com>
2021-04-18 20:31:24 -0500

    v.1.1.1

--- src/controlador/Control.java
+++ src/controlador/Control.java
@@ -5,10 +5,40 @@
  */
 package controlador;
 
-/**
- *
- * @author Equipo3
- */
+import Modelo.Empleado;
+import Modelo.Servicio;
+import vista.Inicio;
+import vista.Registro;
+
+
 public class Control {
+
+    private Empleado empleado;
+    private Servicio servicio;
+    private Inicio inicio;
+    private Registro registro;
+    
+    
+    public void setEmpleado(Empleado empleado) {
+        this.empleado=empleado;
+    }
+
+    public void setServicio(Servicio servicio) {
+        this.servicio=servicio;
+    }
+
+    public void setInicio(Inicio inicio) {
+        this.inicio=inicio;
+    }
+
+
+    public void setRegistro(Registro registro) {
+        this.registro=registro;
+    }
+    
+    public void crearItem (){
+        String [] datos = new String [7];
+        
+    }
     
 }
--- src/laestrellaautolavado/LaEstrellaAutoLavado.java
+++ src/laestrellaautolavado/LaEstrellaAutoLavado.java
@@ -1,11 +1,34 @@
 
 package laestrellaautolavado;
 
+import Modelo.Empleado;
+import Modelo.Servicio;
+import controlador.Control;
+import vista.Inicio;
+import vista.Registro;
+
 
 public class LaEstrellaAutoLavado {
 
   
     public static void main(String[] args) {
+        
+        Control control =new Control();
+        Empleado empleado = new Empleado();
+        Servicio servicio = new Servicio();
+        Inicio inicio =new Inicio();
+        Registro registro = new Registro();
+        
+        empleado.setControl(control);
+        servicio.setControl(control);
+        inicio.setControl(control);
+        registro.setControl(control);
+        
+        control.setEmpleado(empleado);
+        control.setServicio(servicio);
+        control.setInicio(inicio);
+        control.setRegistro(registro);
+        
         
     }
     
